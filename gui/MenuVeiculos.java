b711fc17efec02cbdb441d2eefeeb7b07b4049cf
luscasmartinez <lucasmartinez.aluno@unipampa.edu.br>
2022-12-06 15:54:46 -0300

    06/12/2022 15:54

--- gui/MenuVeiculos.java
+++ gui/MenuVeiculos.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.JOptionPane;
+
+import cadastramentos.CadVeiculos;
+import construtores.Veiculo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -42,10 +46,32 @@ public class MenuVeiculos {
     @FXML
     private TextField valorDiaVeiculo;
 
+    private CadVeiculos listaVeiculos;
+
+    @FXML
+    void confirmarCadastro(ActionEvent event) {
+        if (this.anoVeiculo.getText().equals("") || this.placaVeiculo.getText().equals("") ||this.valorDiaVeiculo.getText().equals("") ) {
+            JOptionPane.showMessageDialog(null,"Todos os campos são obrigatórios.");
+        }else{
+            try {
+                if(listaVeiculos.existe(placaVeiculo.getText())){
+                        JOptionPane.showMessageDialog(null,
+                        "A placa já existe no banco de dados.");
+                }else{
+                        int ano = Integer.parseInt(this.anoVeiculo.getText());
+                        int valorDiaria = Integer.parseInt(this.valorDiaVeiculo.getText());
+                        Veiculo novoVeiculo = new Veiculo(this.placaVeiculo.getText(), ano,valorDiaria);
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+    }
+
     @FXML
     void getTipoVeiculoMV(MouseEvent event) {
 
-    }
+}
 
     @FXML
     void voltarMenuInicial(ActionEvent event) throws IOException {
